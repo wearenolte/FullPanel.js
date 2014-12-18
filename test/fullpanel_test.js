83ef3f98faee9a0a7b7179248984af6f4dd95074
Jeffrey Nolte <jnolte@getmoxied.net>
2014-12-17 19:48:32 -0500

    adds dist, cssmin

--- test/fullpanel_test.js
+++ test/fullpanel_test.js
@@ -20,43 +20,20 @@
       throws(block, [expected], [message])
   */
 
-  module('jQuery#awesome', {
+  module('jQuery#fullpanel', {
     // This will run before each test in this module.
     setup: function() {
-      this.elems = $('#qunit-fixture').children();
+      this.el = $('.fullpane-wrapper');
     }
   });
 
-  test('is chainable', function() {
-    expect(1);
-    // Not a bad test to run on collection methods.
-    strictEqual(this.elems.awesome(), this.elems, 'should be chainable');
+
+  test('does exist', function() {
+      expect(1);
+      ok(this.el, "exists");
+    //strictEqual($.awesome(), 'awesome.', 'should be awesome');
+    //strictEqual($.awesome({punctuation: '!'}), 'awesome!', 'should be thoroughly awesome');
   });
 
-  test('is awesome', function() {
-    expect(1);
-    strictEqual(this.elems.awesome().text(), 'awesome0awesome1awesome2', 'should be awesome');
-  });
-
-  module('jQuery.awesome');
-
-  test('is awesome', function() {
-    expect(2);
-    strictEqual($.awesome(), 'awesome.', 'should be awesome');
-    strictEqual($.awesome({punctuation: '!'}), 'awesome!', 'should be thoroughly awesome');
-  });
-
-  module(':awesome selector', {
-    // This will run before each test in this module.
-    setup: function() {
-      this.elems = $('#qunit-fixture').children();
-    }
-  });
-
-  test('is awesome', function() {
-    expect(1);
-    // Use deepEqual & .get() when comparing jQuery objects.
-    deepEqual(this.elems.filter(':awesome').get(), this.elems.last().get(), 'knows awesome when it sees it');
-  });
 
 }(jQuery));
